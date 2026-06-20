d92b770052668250ab428f9b9bb576c67dc139a3
كجكد شاهر <shaherm2025@gmail.com>
2026-06-20 20:35:19 +0300

    alter expert page aab

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mohammed.SmartAgriAssistant"
         minSdk = 21
         targetSdk = 36
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "4.0"
         multiDexEnabled = true
 
         ndk {
