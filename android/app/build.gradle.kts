876f12b1b75a2ff6c22ab49cdf9094203ea431b5
كجكد شاهر <shaherm2025@gmail.com>
2026-08-11 22:01:14 +0300

    enable R8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
