68cdb0d7dab2b2def80aef247f5b169c5a95ab9c
كجكد شاهر <shaherm2025@gmail.com>
2026-08-12 00:19:14 +0300

    enable R8 with mapping.txt2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,13 @@ android {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
+			
+		    proguardFiles(
+              getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
+                "proguard-rules.pro"
+            )
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
