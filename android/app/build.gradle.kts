fb2545d0e4650283a0f73d1bf8141764dbf19fb9
كجكد شاهر <shaherm2025@gmail.com>
2026-09-08 21:01:11 +0300

    alter token save and use the new farmer_id2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.mohammed.SmartAgriAssistant"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 16
-        versionName = "6.10"
+        versionCode = 17
+        versionName = "6.11"
         multiDexEnabled = true
 
         ndk {
